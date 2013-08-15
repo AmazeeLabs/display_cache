cdac83aa26775b0d0174387e83c903a451c4707a
Caseledde <Caseledde@887940.no-reply.drupal.org>
2013-08-15 18:39:38 +0200

    by Caseledde: Remove CSRF vulnerability. Improve documentation of hooks.

--- display_cache.api.php
+++ display_cache.api.php
@@ -12,6 +12,8 @@
 /**
  * Alters the keys to build a cid.
  *
+ * Use this alteration to edit the context of cached entities.
+ *
  * @param array $keys
  *   Array of keys. Used to build the cache id.
  *
@@ -19,7 +21,10 @@
  * @see drupal_render_cid_parts()
  */
 function hook_display_cache_cache_keys_alter(&$keys) {
-
+  // Add a 'foo' context to all nodes.
+  if ($keys['entity_type'] === 'node') {
+    $keys['my_new_context'] = 'foo';
+  }
 }
 
 /**
